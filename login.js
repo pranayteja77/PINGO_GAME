298c82d85cab3f4c1d5cd97a317e25dd0dadedfb
Pranay Teja S <pranay.s@sreetechinc.com>
2025-06-24 18:01:03 +0530

    third changes

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ function validateLogin(event) {
     let password = document.getElementById("password").value;
 
     if (username === "admin" && password === "admin") {
-        window.location.href = "index.html"; // Redirect to Pingo game
+        window.location.href = "home.html"; // Redirect to Pingo game
     } else {
         document.getElementById("error-msg").style.display = "block"; // Show error message
     }
